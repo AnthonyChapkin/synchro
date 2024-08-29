ddc3255ae54b040a0e618cebab24b41babceabc9
Anthony Chapkin <anthonyvchapkin@gmail.com>
2024-08-28 21:04:18 -0700

    Make button on sign in and sign up page change on hover

--- client/src/components/SignInForm.tsx
+++ client/src/components/SignInForm.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function SignInForm() {
+  const [signInButtonHovered, setSignInButtonHovered] = useState(false);
+  const [goToSignUpbuttonHovered, setGoToSignUpbuttonHovered] = useState(false);
+
   return (
     <form className="sign-in-form">
       <label className="sign-in-form-label">Sign In</label>
@@ -20,12 +24,36 @@ function SignInForm() {
         className="sign-in-text-field"
         id="sign-in-password-field-id"
       ></input>
-      <input type="button" className="sign-in-button" value="Sign In" />
+      <input
+        type="button"
+        className={
+          signInButtonHovered ? "sign-in-button hovered" : "sign-in-button"
+        }
+        value="Sign In"
+        onClick={() => console.log("Signed In")}
+        onMouseOver={() => {
+          setSignInButtonHovered(true);
+        }}
+        onMouseOut={() => {
+          setSignInButtonHovered(false);
+        }}
+      />
       <label className="or-label">OR</label>
       <input
         type="button"
-        className="go-to-sign-up-button"
+        className={
+          goToSignUpbuttonHovered
+            ? "go-to-sign-up-button hovered"
+            : "go-to-sign-up-button"
+        }
         value="Go to Sign Up Page"
+        onClick={() => console.log("Went to Sign Up")}
+        onMouseOver={() => {
+          setGoToSignUpbuttonHovered(true);
+        }}
+        onMouseOut={() => {
+          setGoToSignUpbuttonHovered(false);
+        }}
       ></input>
       <Link to="/sign-up">Go to Sign Up Page</Link>
     </form>
--- client/src/components/SignUpForm.tsx
+++ client/src/components/SignUpForm.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function SignUpForm() {
+  const [signUpButtonHovered, setSignUpButtonHovered] = useState(false);
+  const [goToSignInbuttonHovered, setGoToSignInbuttonHovered] = useState(false);
+
   return (
     <form className="sign-up-form">
       <label className="sign-up-form-label">Sign Up</label>
@@ -20,12 +24,28 @@ function SignUpForm() {
         className="sign-up-text-field"
         id="sign-up-password-field-id"
       ></input>
-      <input type="button" className="sign-up-button" value="Sign Up" />
+      <input
+        type="button"
+        className={
+          signUpButtonHovered ? "sign-up-button hovered" : "sign-up-button"
+        }
+        value="Sign Up"
+        onClick={() => console.log("Signed Up")}
+        onMouseOver={() => setSignUpButtonHovered(true)}
+        onMouseOut={() => setSignUpButtonHovered(false)}
+      />
       <label className="or-label">OR</label>
       <input
         type="button"
-        className="go-to-sign-in-button"
+        className={
+          goToSignInbuttonHovered
+            ? "go-to-sign-in-button hovered"
+            : "go-to-sign-in-button"
+        }
         value="Go to Sign In Page"
+        onClick={() => console.log("Went to Sign In")}
+        onMouseOver={() => setGoToSignInbuttonHovered(true)}
+        onMouseOut={() => setGoToSignInbuttonHovered(false)}
       ></input>
       <Link to="/">Go to Sign In Page</Link>
     </form>
